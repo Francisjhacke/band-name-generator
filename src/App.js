bf04726fda49abe4ea42b882592b3fb13ace80c8
Francis <francisjhackenberger@gmail.com>
2018-06-14 20:28:50 -0400

    Created a container class to hold the generate button and random band name field for easier prop passing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import './App.css';
 
 // https://www.datamuse.com/api/ --- API FOR GETTING RANDOM WORDS
 
-import GenerateButton from './components/GenerateButton';
-import AppHeader from './components/AppHeader';
-import {GenerateBandName} from './helpers.js';
-
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+import RandomBandNameContainer from './components/RandomBandNameContainer';
 
 const cardStyle = {
   textAlign : 'center'
@@ -21,19 +19,18 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Card style={cardStyle}>
-        <CardContent>
-          <Typography variant="headline" component="h2">
-            Random Band Name Generator
-          </Typography>
-          <Typography component="p">
-            <span>Click the 'Generate' Button below to generate your random band name!</span><br/>
-            <GenerateButton/><br/>
-            <span>{GenerateBandName()}</span> <br />
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
+        <Card style={cardStyle}>
+          <CardContent>
+            <Typography variant="headline" component="h2">
+              Random Band Name Generator
+            </Typography>
+            <Typography component="p">
+              <span>Click the 'Generate' Button below to generate your random band name!</span><br/>
+              <RandomBandNameContainer/>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     );
   }
 }
