d80ba4d0aa258c2268e802065085b195dec15ae2
Francis <francisjhackenberger@gmail.com>
2018-06-14 20:13:43 -0400

    Using material-ui card styling as a container for the main app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import './App.css';
 
 import GenerateButton from './components/GenerateButton';
 import AppHeader from './components/AppHeader';
-
 import {GenerateBandName} from './helpers.js';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const cardStyle = {
+  textAlign : 'center'
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <AppHeader/>
-        <p className="App-intro">
-          To get started, click the <strong>Generate</strong> button.<br/>
-          <GenerateButton/>
-          <span>{GenerateBandName()}</span>
-        </p>
-      </div>     
+      <Card style={cardStyle}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            Random Band Name Generator
+          </Typography>
+          <Typography component="p">
+            <span>Click the 'Generate' Button below to generate your random band name!</span><br/>
+            <GenerateButton/><br/>
+            <span>{GenerateBandName()}</span> <br />
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
     );
   }
 }
